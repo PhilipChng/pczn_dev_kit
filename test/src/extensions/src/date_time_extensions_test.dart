2b39440e46ee1be7abf3eb611bd4bbe94686dad7
PhilipChng <pczn0327@gmail.com>
2024-06-12 11:19:29 +0800

    [DateTimeExtension] Null will return empty String

--- test/src/extensions/src/date_time_extensions_test.dart
+++ test/src/extensions/src/date_time_extensions_test.dart
@@ -3,13 +3,19 @@ import 'package:test/test.dart';
 
 void main() {
   group('DateTimeExtension', () {
-    test('ddMMyyyy', () {
-      final now = DateTime.now();
-      final day = now.day.toString().padLeft(2, '0');
-      final month = now.month.toString().padLeft(2, '0');
-      final year = now.year.toString().padLeft(4, '0');
-      expect(now.ddMMyyyy, isNotEmpty);
-      expect(now.ddMMyyyy, '$day/$month/$year');
+    group('ddMMyyyy', () {
+      test('with null', () {
+        expect(null.ddMMyyyy, '');
+      });
+
+      test('with Actual Date Time', () {
+        final now = DateTime.now();
+        final day = now.day.toString().padLeft(2, '0');
+        final month = now.month.toString().padLeft(2, '0');
+        final year = now.year.toString().padLeft(4, '0');
+        expect(now.ddMMyyyy, isNotEmpty);
+        expect(now.ddMMyyyy, '$day/$month/$year');
+      });
     });
   });
 }
